e9169dab7eaf8e76a13560569d7bb511fba68a48
Ionut Grecu <ionut@grecu.eu>
2021-02-19 22:11:36 +0200

    jetstream init

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,33 +1,27 @@
 <?php
 
-use App\Models\Product;
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration {
-
+class CreateUsersTable extends Migration
+{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up() {
-        $tableName = (new User)->getTable();
-
-        if (Schema::hasTable($tableName)) {
-            echo "$tableName already exists. Migrate " . __FILE__ . " manually";
-            return;
-        }
-
-        Schema::create($tableName, function (Blueprint $table) {
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->text('profile_photo_path')->nullable();
             $table->timestamps();
         });
     }
@@ -37,15 +31,8 @@ class CreateUsersTable extends Migration {
      *
      * @return void
      */
-    public function down() {
-        $tableName = (new User)->getTable();
-
-        if (Product::count()) {
-            echo "Can't rollback " . __FILE__ . " . The table $tableName has rows.";
-            return;
-        }
-
-        Schema::dropIfExists($tableName);
+    public function down()
+    {
+        Schema::dropIfExists('users');
     }
-
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::group(['prefix'=>'admin'], function(){
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
