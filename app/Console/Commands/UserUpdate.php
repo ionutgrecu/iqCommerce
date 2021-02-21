08a136c3bb61523edf8eb9fff7bd826aba01a7c9
Ionut Grecu <ionut@grecu.eu>
2021-02-21 17:42:53 +0200

    docker compose mysql mix

---
+++ app/Console/Commands/UserUpdate.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Hash;
+use Illuminate\Console\Command;
+use Illuminate\Foundation\Auth\User;
+use Str;
+
+/** @version 1.0.0
+ * @author ionut
+ * @copyright Copyright (C) 2019 Ionut Grecu
+ * @license Royalty Free
+ * @requires PHP7
+ * @package 
+ * @subpackage 
+ */
+class UserUpdate extends Command {
+
+    protected $signature = 'cli:user-update {email} {--name=} {--password=}';
+    protected $description = 'Create or update user. --name --password';
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function handle() {
+        $user = User::firstOrNew([
+                    'email' => $this->argument('email'),
+        ]);
+
+        if (!$user)
+            $user = new User;
+
+        if ($this->option('name'))
+            $user->name = $this->option('name');
+
+        if (null !== $this->option('password')) {
+            if ($this->option('password'))
+                $user->password = Hash::make($this->option('password'));
+            else
+                $user->password = Hash::make($this->ask('Input the password', Str::random(8)));
+        }
+
+        if (!$user->password) {
+            $password = Str::random(8);
+            $user->password = Hash::make($password);
+            $this->info('Password: ' . $password);
+        }
+
+        $user->save();
+    }
+
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\UserUpdate::class,
+        Commands\Test::class,
     ];
 
     /**
