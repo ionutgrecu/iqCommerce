b4e7acb62a6b5a074d8cfb57acdc6b7630e91dd3
Ionut Grecu <ionut@grecu.eu>
2021-02-23 22:43:57 +0200

    theme implement

--- resources/views/admin/_partials/nav.blade.php
+++ resources/views/admin/_partials/nav.blade.php
@@ -3,24 +3,12 @@
     <img class="c-sidebar-brand-minimized" src="{{ url('assets/admin/brand/coreui-signet-white.svg') }}" width="118" height="46" alt="CoreUI Logo">
 </div>
 <ul class="c-sidebar-nav ps ps--active-y">
-    <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="index.html">
+    <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="/admin">
             <svg class="c-sidebar-nav-icon">
             <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-speedometer') ?>"></use>
             </svg> Dashboard<span class="badge badge-info">NEW</span></a></li>
-    <li class="c-sidebar-nav-title">Theme</li>
-    <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="colors.html">
-            <svg class="c-sidebar-nav-icon">
-            <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-drop') ?>"></use>
-            </svg> Colors</a></li>
-    <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="typography.html">
-            <svg class="c-sidebar-nav-icon">
-            <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-pencil') ?>"></use>
-            </svg> Typography</a></li>
-    <li class="c-sidebar-nav-title">Components</li>
     <li class="c-sidebar-nav-item c-sidebar-nav-dropdown"><a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-            <svg class="c-sidebar-nav-icon">
-            <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-puzzle') ?>"></use>
-            </svg> Base</a>
+            <i class="cid-store"></i> Base</a>
         <ul class="c-sidebar-nav-dropdown-items">
             <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="base/breadcrumb.html"><span class="c-sidebar-nav-icon"></span> Breadcrumb</a></li>
             <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="base/cards.html"><span class="c-sidebar-nav-icon"></span> Cards</a></li>
@@ -40,78 +28,6 @@
             <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="base/tooltips.html"><span class="c-sidebar-nav-icon"></span> Tooltips</a></li>
         </ul>
     </li>
-    <li class="c-sidebar-nav-item c-sidebar-nav-dropdown"><a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-            <svg class="c-sidebar-nav-icon">
-            <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-cursor') ?>"></use>
-            </svg> Buttons</a>
-        <ul class="c-sidebar-nav-dropdown-items">
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="buttons/buttons.html"><span class="c-sidebar-nav-icon"></span> Buttons</a></li>
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="buttons/button-group.html"><span class="c-sidebar-nav-icon"></span> Buttons Group</a></li>
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="buttons/dropdowns.html"><span class="c-sidebar-nav-icon"></span> Dropdowns</a></li>
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="buttons/brand-buttons.html"><span class="c-sidebar-nav-icon"></span> Brand Buttons</a></li>
-        </ul>
-    </li>
-    <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="charts.html">
-            <svg class="c-sidebar-nav-icon">
-            <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-chart-pie') ?>"></use>
-            </svg> Charts</a></li>
-    <li class="c-sidebar-nav-dropdown"><a class="c-sidebar-nav-dropdown-toggle" href="#">
-            <svg class="c-sidebar-nav-icon">
-            <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-star') ?>"></use>
-            </svg> Icons</a>
-        <ul class="c-sidebar-nav-dropdown-items">
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="icons/coreui-icons-free.html"> CoreUI Icons<span class="badge badge-success">Free</span></a></li>
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="icons/coreui-icons-brand.html"> CoreUI Icons - Brand</a></li>
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="icons/coreui-icons-flag.html"> CoreUI Icons - Flag</a></li>
-        </ul>
-    </li>
-    <li class="c-sidebar-nav-item c-sidebar-nav-dropdown"><a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-            <svg class="c-sidebar-nav-icon">
-            <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-bell') ?>"></use>
-            </svg> Notifications</a>
-        <ul class="c-sidebar-nav-dropdown-items">
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="notifications/alerts.html"><span class="c-sidebar-nav-icon"></span> Alerts</a></li>
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="notifications/badge.html"><span class="c-sidebar-nav-icon"></span> Badge</a></li>
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="notifications/modals.html"><span class="c-sidebar-nav-icon"></span> Modals</a></li>
-        </ul>
-    </li>
-    <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="widgets.html">
-            <svg class="c-sidebar-nav-icon">
-            <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-calculator') ?>"></use>
-            </svg> Widgets<span class="badge badge-info">NEW</span></a></li>
-    <li class="c-sidebar-nav-divider"></li>
-    <li class="c-sidebar-nav-title">Extras</li>
-    <li class="c-sidebar-nav-item c-sidebar-nav-dropdown"><a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-            <svg class="c-sidebar-nav-icon">
-            <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-star') ?>"></use>
-            </svg> Pages</a>
-        <ul class="c-sidebar-nav-dropdown-items">
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="login.html" target="_top">
-                    <svg class="c-sidebar-nav-icon">
-                    <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-account-logout') ?>"></use>
-                    </svg> Login</a></li>
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="register.html" target="_top">
-                    <svg class="c-sidebar-nav-icon">
-                    <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-account-logout') ?>"></use>
-                    </svg> Register</a></li>
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="404.html" target="_top">
-                    <svg class="c-sidebar-nav-icon">
-                    <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-bug') ?>"></use>
-                    </svg> Error 404</a></li>
-            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="500.html" target="_top">
-                    <svg class="c-sidebar-nav-icon">
-                    <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-bug') ?>"></use>
-                    </svg> Error 500</a></li>
-        </ul>
-    </li>
-    <li class="c-sidebar-nav-item mt-auto"><a class="c-sidebar-nav-link c-sidebar-nav-link-success" href="https://coreui.io" target="_top">
-            <svg class="c-sidebar-nav-icon">
-            <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-cloud-download') ?>"></use>
-            </svg> Download CoreUI</a></li>
-    <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link c-sidebar-nav-link-danger" href="https://coreui.io/pro/" target="_top">
-            <svg class="c-sidebar-nav-icon">
-            <use xlink:href="<?= url('/assets/admin/icons/sprites/free.svg#cil-layers') ?>"></use>
-            </svg> Try CoreUI<strong>PRO</strong></a></li>
     <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; height: 576px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 453px;"></div></div></ul>
 <button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent" data-class="c-sidebar-minimized"></button>
 </div>
