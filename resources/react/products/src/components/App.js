1c7e1c0ce06be4b3691066fcbb377a127f22af9a
Ionut Grecu <ionut@grecu.eu>
2021-02-25 23:24:27 +0200

    react products

---
+++ resources/react/products/src/components/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+import React from 'react';
+import Main from './Main'
+
+class App extends React.Component{
+    render(){
+        return <Main />
+    }
+}
+
+export default App;
---
+++ resources/react/products/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Main extends React.Component {
+    constructor() {
+        super()
+
+        this.state={
+
+        }
+    }
+
+    render() {
+        return <div>Main</div>
+    }
+} export default Main;
