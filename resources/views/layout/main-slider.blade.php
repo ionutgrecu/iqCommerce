252d717895513af11934fd94deafd322b9fbf7a2
Ionut Grecu <ionut@grecu.eu>
2021-03-23 13:34:54 +0200

    theme: homepage slider

---
+++ resources/views/layout/main-slider.blade.php
@@ -0,0 +1,112 @@
+<div class="parvez_revslider_element parvez_content_element home-slider layout1">
+    <div class="rev_slider_wrapper fullwidthbanner-container" style="margin:0px auto;background-color:#bddde5;padding:0px;margin-top:0px;margin-bottom:0px;">
+        <!-- START REVOLUTION SLIDER -->
+        <div id="rev_slider_digital_1" class="rev_slider fullwidthabanner" style="display:none;" >
+            <ul>
+                <!-- SLIDE  -->
+                <li data-index="rs-1" data-transition="random" data-slotamount="9" data-masterspeed="600" data-saveperformance="on" data-thumb="<?=asset('assets/banners/homepage1-100x50.jpg')?>"
+                    data-rotate="0" data-title="Slide-1" data-description="">
+                    <!-- MAIN IMAGE -->
+                    <img  src="<?=asset('assets/banners/homepage1.jpg')?>" alt="" width="1170" height="465" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
+                    <!-- LAYERS -->
+                    <!-- LAYER NR. 1 -->
+                    <div class="tp-caption lfr lfr text1 tp-resizeme" id="slide-1-layer-1" 
+                         data-x="569" 
+                         data-y="79" 
+                         data-width="auto" 
+                         data-height="auto" 
+                         data-speed="1500"
+                         data-easing="Power3.easeInOut" 
+                         data-start="500"
+                         data-splitin="none" 
+                         data-splitout="none" 
+                         data-responsive_offset="on" 
+                         style="z-index: 5; white-space: nowrap;border-color:rgba(255, 255, 255, 1.00);">Give You a Beautiful look !
+                    </div>
+                    <!-- LAYER NR. 2 -->
+                    <div class="tp-caption lfr lfr thintext_dark tp-resizeme" id="slide-1-layer-2" data-x="543" 
+                         data-y="129" 
+                         data-width="auto" 
+                         data-height="auto" 
+                         data-speed="1500"
+                         data-easing="Power3.easeInOut" 
+                         data-start="1000" 
+                         data-splitin="none" 
+                         data-splitout="none" 
+                         data-responsive_offset="on" 
+                         style="z-index: 6; white-space: nowrap; color: rgba(0, 0, 0, 1.00);">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    </div>
+                    <!-- LAYER NR. 3 -->
+                    <div class="tp-caption lfr lfr black tp-resizeme" id="slide-1-layer-3" 
+                         data-x="764" 
+                         data-y="174" 
+                         data-width="auto" 
+                         data-height="auto" 
+                         data-speed="1500" 
+                         data-easing="Power3.easeInOut" 
+                         data-start="1500" 
+                         data-splitin="none" 
+                         data-splitout="none" 
+                         data-responsive_offset="on" style="z-index: 7; white-space: nowrap;"><a href='#' class='tp-button'>View more</a> 
+                    </div>
+                </li>
+                <!-- SLIDE  -->
+                <li data-index="rs-5" data-transition="random" data-slotamount="9" data-masterspeed="600" data-saveperformance="on" 
+                    data-thumb="<?=asset('assets/banners/homepage2-100x50.jpg')?>"
+                    data-rotate="0"
+                    data-title="Slide-2" 
+                    data-description="">
+                    <!-- MAIN IMAGE -->
+                    <img  src="<?=asset('assets/banners/homepage2.jpg')?>" alt="" width="1170" height="466" 
+                          data-bgposition="center top" 
+                          data-bgfit="cover" 
+                          data-bgrepeat="no-repeat" class="rev-slidebg" 
+                          data-no-retina>
+                    <!-- LAYERS -->
+                    <!-- LAYER NR. 1 -->
+                    <div class="tp-caption lfl lfl text1 tp-resizeme" id="slide-5-layer-1" 
+                         data-x="135" 
+                         data-y="79" 
+                         data-width="auto" 
+                         data-height="auto" 
+                         data-speed="1500"
+                         data-easing="Power3.easeInOut" 
+                         data-start="500"
+                         data-splitin="none" 
+                         data-splitout="none" 
+                         data-responsive_offset="on" style="z-index: 5; white-space: nowrap;border-color:rgba(255, 255, 255, 1.00);">Give You a Beautiful look !
+                    </div>
+                    <!-- LAYER NR. 2 -->
+                    <div class="tp-caption lfl lfl thintext_dark tp-resizeme" id="slide-5-layer-2" 
+                         data-x="100" 
+                         data-y="129" 
+                         data-width="auto" 
+                         data-height="auto" 
+                         data-speed="1500"
+                         data-easing="Power3.easeInOut" 
+                         data-start="1000" 
+                         data-splitin="none" 
+                         data-splitout="none" 
+                         data-responsive_offset="on" 
+                         style="z-index: 6; white-space: nowrap; color: rgba(0, 0, 0, 1.00);">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    </div>
+                    <!-- LAYER NR. 3 -->
+                    <div class="tp-caption lfl lfl black tp-resizeme" id="slide-5-layer-3" 
+                         data-x="291" 
+                         data-y="177" 
+                         data-width="auto" 
+                         data-height="auto" 
+                         data-speed="1500" 
+                         data-easing="Power3.easeInOut" 
+                         data-start="1500" 
+                         data-splitin="none" 
+                         data-splitout="none" 
+                         data-responsive_offset="on" 
+                         style="z-index: 7; white-space: nowrap;"><a href='#' class='tp-button'>View more</a> 
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- END REVOLUTION SLIDER -->
+</div>
